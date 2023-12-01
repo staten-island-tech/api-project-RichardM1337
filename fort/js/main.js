ead0ac7433ab3911b57708fec32a8ca3080b8238
xX_EpicHackerDude_Xx <113366399+RichardM1337@users.noreply.github.com>
2023-12-01 13:15:52 -0500

    what

--- fort/js/main.js
+++ fort/js/main.js
@@ -1,27 +1,29 @@
 import "../css/background.css" assert { type: "css" };
+import "../css/style.css" assert { type: "css" };
 
 const API = `https://fortnite-api.com/v2/cosmetics/br`;
 import { DOMSelectors } from "./domselectors";
-/* async function getCosmetics(API) {
+
+async function getCosmetics(API) {
   try {
     const response = await fetch(API);
 
     const data = await response.json();
     const cosmeticsObject = data.data;
-    cosmeticsObject.forEach((i) => {
+    cosmeticsObject.slice(0, 50).forEach((i) => {
       const card = document.createElement("div");
       card.classList.add("card");
       card.innerHTML = `
-            <img src="${i.images.icon}" class="movieimg" alt="${i.name}">
-            <h1 class="name">${i.name}</h1>
-            <p class="itemtype"> ${i.type.displayValue}</p>
-            <p class="rarity"> ${i.rarity.displayValue}</p>`;
-      DOMSelectors.moviecontainer.appendChild(card);
+                <img src="${i.images.icon}" class="itemicon" alt="${i.name}">
+                <h1 class="name">${i.name}</h1>
+                <p class="itemtype"> ${i.type.displayValue}</p>
+                <p class="rarity"> ${i.rarity.displayValue}</p>`;
+      card.classList.add(`${i.rarity.displayValue}`);
+      DOMSelectors.itemcontainer.appendChild(card);
     });
   } catch (error) {}
 }
 
 getCosmetics(API);
- */
 
 /* need to limit this so ur website doesnt crash loser */
