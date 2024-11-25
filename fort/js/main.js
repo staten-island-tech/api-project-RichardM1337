b448edd41682e4bbf51d34e4b4178607ec913a7f
RichardM1337 <richardm246@nycstudents.net>
2024-11-24 22:51:10 -0500

    api fix

--- fort/js/main.js
+++ fort/js/main.js
@@ -2,13 +2,12 @@ import "../css/background.css" assert { type: "css" };
 import "../css/style.css" assert { type: "css" };
 import { DOMSelectors } from "./domselectors";
 const form = DOMSelectors.itemSearchForm;
-function cardCreator(arr) {
+function cardCreator(i) {
   DOMSelectors.itemcontainer.innerHTML = "";
-  arr.forEach((i) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
-      <img src="${i.images.icon}" class="itemicon" alt="Picture of ${i.name}">
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+      <img src="${i.images["icon"]}" class="itemicon" alt="Picture of ${i.name}">
       <h2 tabindex="0" class="itemname">${i.name}</h1>
       <div class="dropdownMenu">
         <details>
@@ -17,42 +16,37 @@ function cardCreator(arr) {
           </summary>
           <p tabindex="0" class="description">Description: "${i.description}"</p>
           <p tabindex="0" class="itemtyperarity"> ${i.rarity.displayValue} ${i.type.displayValue}</p>
-          <p tabindex="0" class="partOfSet">${i.set.text}</p>
-          <p tabindex="0" class="introduced">${i.introduction.text}</p>
+          <p tabindex="0" class="partOfSet">${i.set}</p>
+          <p tabindex="0" class="introduced">${i.introduction}</p>
       </div>`;
-    DOMSelectors.itemcontainer.appendChild(card);
-    card.classList.add(`${i.rarity.value}`);
-    card.id = `${i.type.value}`;
-  });
+  DOMSelectors.itemcontainer.appendChild(card);
+  card.classList.add(`${i.rarity.value}`);
+  card.id = `${i.type.value}`;
 }
 
 async function getCosmetics() {
   try {
-    const response = await fetch(
-      `https://fortnite-api.com/v2/cosmetics/new`
-    );
+    const response = await fetch(`https://fortnite-api.com/v2/cosmetics/new`);
     const data = await response.json();
     const cosmeticsObject = data.data.items;
-    cardCreator(cosmeticsObject);
+    console.log(cosmeticsObject.br[0]);
+    cosmeticsObject.br.forEach((cosmetic) => cardCreator(cosmetic));
     if (data.status != 200) {
       throw new Error(data.error);
     }
   } catch (error) {
-    if (error.includes("TypeError")) {
-      // identifies i.introduction etc as html element instead of iterating attempt
-      console.log("false typeerror");
-    } else {
-      DOMSelectors.h1.innerHTML = error;
-    }
+    console.error(error);
+    DOMSelectors.h1.innerHTML = error;
   }
 }
 async function cosmeticsSearch(input) {
   try {
     const response = await fetch(
-      `https://fortnite-api.com/v2/cosmetics/br/search/all/?name=${input}`
+      `https://fortnite-api.com/v2/cosmetics/br/search?name=${input}`
     );
     const data = await response.json();
     const cosmeticsObject = data.data;
+    console.log(cosmeticsObject.id);
     DOMSelectors.h1.innerHTML = "Fortnite API";
     DOMSelectors.itemcontainer.innerHTML = "";
     cardCreator(cosmeticsObject);
